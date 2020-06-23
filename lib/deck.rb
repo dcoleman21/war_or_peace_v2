2f66779a234507676c277f90f0296d3c0d0e76f2
Dani Coleman <danicoleman00@gmail.com>
2020-06-23 15:35:55 -0600

    Add card and deck class files

---
+++ lib/deck.rb
@@ -0,0 +1,35 @@
+class Deck
+  attr_reader :cards
+
+  def initialize(cards)
+    @cards = cards
+  end
+
+  def rank_of_card_at(index)
+    cards[index].rank
+  end
+
+  def high_ranking_cards
+    found_high = []
+    cards.each do |card|
+      if card.rank >= 11
+        found_high << card
+      end
+    end
+    found_high
+  end
+
+  def percent_high_ranking
+    total_card_count = cards.count
+    high_cards_count = high_ranking_cards.count
+    (high_cards_count.to_f / total_card_count * 100).round(2)
+  end
+
+  def remove_card(card)
+    @cards.shift
+  end
+
+  def add_card(card)
+    @cards << card
+  end
+end
