574c6eae65e7831d80d3b3cb76c2ab41180cae03
Dani Coleman <danicoleman00@gmail.com>
2020-06-24 15:06:33 -0600

    Rename tests for better readability

--- test/deck_test.rb
+++ test/deck_test.rb
@@ -9,7 +9,6 @@ class DeckTest < Minitest::Test
     @card1 = Card.new(:diamond, 'Queen', 12)
     @card2 = Card.new(:spade, '3', 3)
     @card3 = Card.new(:heart, 'Ace', 14)
-    @card4 = Card.new(:club, '5', 5)
     @cards = [@card1, @card2, @card3]
     @deck = Deck.new(@cards)
   end
@@ -22,58 +21,64 @@ class DeckTest < Minitest::Test
     assert_includes @deck.cards, @card3
   end
 
-  def test_it_can_get_rank_of_card
+  def test_rank_of_card_at_returns_card_rank
     assert_equal 12, @deck.rank_of_card_at(0)
     assert_equal 14, @deck.rank_of_card_at(2)
   end
 
-  def test_it_can_get_high_ranking_cards_from_original_deck
+  def test_high_ranking_cards_returns_high_rank_card
     assert_includes @deck.high_ranking_cards, @card1
     assert_includes @deck.high_ranking_cards, @card3
+    assert_equal 2, @deck.high_ranking_cards.count
   end
 
-  def test_it_can_get_percent_high_ranking_from_original_deck
+  def test_high_ranking_cards_does_not_return_removed_card
+    assert_includes @deck.high_ranking_cards, @card1
+    @deck.remove_card(@card1)
+
+    refute_includes @deck.high_ranking_cards, @card1
+  end
+
+  def test_percent_high_ranking_returns_percent_as_float
     assert_equal 66.67, @deck.percent_high_ranking
   end
 
-  def test_it_can_remove_cards
+  def test_percent_high_ranking_returns_new_percent
+    assert_equal 66.67, @deck.percent_high_ranking
+    @deck.remove_card(@card1)
+
+    assert_equal 50.0, @deck.percent_high_ranking
+  end
+
+  def test_remove_card_removes_card
     assert_equal 3, @deck.cards.count
     @deck.remove_card(@card1)
+
     assert_equal 2, @deck.cards.count
     refute_includes @deck.cards, @card1
   end
 
-  def test_it_can_get_high_ranking_cards_after_card_removed #so we don't have to specify what card is the high ranking now? @card3
-    assert_includes @deck.high_ranking_cards, @card1
-    @deck.remove_card(@card1)
-    refute_includes @deck.high_ranking_cards, @card1
-  end
+  def test_add_card_adds_card
+    assert_equal 3, @deck.cards.count
+    new_card = Card.new(:club, '5', 5)
+    refute_includes @deck.cards, new_card
+    @deck.add_card(new_card)
 
-  def test_it_can_get_percent_high_ranking_after_card_removed
-    assert_equal 66.67, @deck.percent_high_ranking
-    @deck.remove_card(@card1)
-    assert_equal 50.0, @deck.percent_high_ranking
-  end
-
-  def test_it_can_add_cards
-    @deck.remove_card(@card1)
-    @card4 = Card.new(:club, '5', 5)
-    @deck.add_card(@card4)
-    assert_equal 3, @cards.count
+    assert_includes @deck.cards, new_card
+    assert_equal 4, @cards.count
   end
 
   def test_it_can_get_high_ranking_with_new_card_added
-    @deck.remove_card(@card1)
-    @deck.add_card(@card4)
+    new_card = Card.new(:club, '5', 5)
+    @deck.add_card(new_card)
     assert_includes @deck.high_ranking_cards, @card3
   end
 
   def test_add_card_affects_percent_high_ranking
-    @deck.remove_card(@card1)
-    @deck.add_card(@card4)
-    assert_includes @deck.cards, @card2
-    assert_includes @deck.cards, @card3
-    assert_includes @deck.cards, @card4
-    assert_equal 33.33, @deck.percent_high_ranking
+    assert_equal 66.67, @deck.percent_high_ranking
+    new_card = Card.new(:club, '5', 5)
+    @deck.add_card(new_card)
+
+    assert_equal 50.0, @deck.percent_high_ranking
   end
 end
