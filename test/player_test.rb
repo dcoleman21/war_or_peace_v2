1df0855b7d8f45638a24952e1160331ce492c146
Dani Coleman <danicoleman00@gmail.com>
2020-06-24 10:11:26 -0600

    Add player test

---
+++ test/player_test.rb
@@ -0,0 +1,53 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/card'
+require './lib/deck'
+require './lib/player'
+
+class PlayerTest < Minitest::Test
+
+  def setup
+    @card1 = Card.new(:diamond, 'Queen', 12)
+    @card2 = Card.new(:spade, '3', 3)
+    @card3 = Card.new(:heart, 'Ace', 14)
+    @deck = Deck.new([@card1, @card2, @card3])
+    @player = Player.new('Clarisa', @deck)
+  end
+
+  def test_it_has_readable_attributes
+    assert_instance_of Player, @player
+    assert_equal 'Clarisa', @player.name
+    assert_equal @deck, @player.deck
+  end
+
+  def test_it_has_lost
+    assert_equal false, @player.has_lost?
+  end
+
+  def test_has_lost_can_return_true
+    assert_equal @deck, @player.deck
+    @player.deck.remove_card(@card1)
+    assert_equal false, @player.has_lost?
+    @player.deck.remove_card(@card2)
+    assert_equal false, @player.has_lost?
+    @player.deck.remove_card(@card3)
+    assert_equal true, @player.has_lost?
+    assert_equal @deck, @player.deck
+  end
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# player.deck
+# #=> #<Deck:0x007f9cc396bdf8 @cards=[]>
