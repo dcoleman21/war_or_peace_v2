21e3eec3b06d4a447b57f709f6a8051dcbb01bf1
Dani Coleman <danicoleman00@gmail.com>
2020-06-24 09:20:25 -0600

    Refactor per Josh suggestions

--- test/deck_test.rb
+++ test/deck_test.rb
@@ -27,13 +27,19 @@ class DeckTest < Minitest::Test
     assert_equal 14, @deck.rank_of_card_at(2)
   end
 
-  def test_it_can_get_high_ranking_cards
+  def test_it_can_get_high_ranking_cards #when I use assert_includes it gives me a failure
     assert_equal [@card1, @card2, @card3], @deck.cards
     assert_equal [@card1, @card3], @deck.high_ranking_cards
+    @deck.remove_card(@card1)
+    assert_equal [@card3], @deck.high_ranking_cards
   end
 
   def test_it_can_get_percent_high_ranking
     assert_equal 66.67, @deck.percent_high_ranking
+    @deck.remove_card(@card1)
+    assert_equal 50.0, @deck.percent_high_ranking
+    @deck.add_card(@card4)
+    assert_equal 33.33, @deck.percent_high_ranking
   end
 
   def test_it_can_remove_cards
@@ -42,15 +48,16 @@ class DeckTest < Minitest::Test
     assert_equal 2, @deck.cards.count
     refute_includes @deck.cards, @card1
     assert_equal [@card2, @card3], @deck.cards
-    assert_equal [@card3], @deck.high_ranking_cards
-    assert_equal 50.0, @deck.percent_high_ranking
+    # assert_equal [@card3], @deck.high_ranking_cards
+    # assert_equal 50.0, @deck.percent_high_ranking
   end
 
   def test_it_can_add_cards
+    @deck.remove_card(@card1)
     @card4 = Card.new(:club, '5', 5)
     @deck.add_card(@card4)
     assert_equal [@card2, @card3, @card4], @deck.cards
-    assert_equal [@card3], @deck.high_ranking_cards
-    assert_equal 33.33, @deck.percent_high_ranking
+    # assert_equal [@card3], @deck.high_ranking_cards
+    # assert_equal 33.33, @deck.percent_high_ranking
   end
 end
