10921047733100df5107a4cfa9fd142a01e71988
Dani Coleman <danicoleman00@gmail.com>
2020-06-25 10:55:15 -0600

    Add refactors, war and mad tests

--- test/turn_test.rb
+++ test/turn_test.rb
@@ -30,18 +30,15 @@ class TurnTest < Minitest::Test
     assert_equal @player2, @turn.player2
   end
 
-  def test_it_exists_with_deck_attributes  
+  def test_it_exists_with_deck_attributes
     assert_instance_of Deck, @deck1
-    assert_equal 4, @turn.deck1.cards, @card1
-    assert_includes @turn.deck1.cards, @card2
-    assert_includes @turn.deck1.cards, @card5
-    assert_includes @turn.deck1.cards, @card8
+    assert_equal 4, @turn.player1.deck.cards.count
+
     assert_instance_of Deck, @deck2
-    assert_equal 4, @turn.deck2.cards.count
-    assert_includes @turn.deck2.cards, @card3
-    assert_includes @turn.deck2.cards, @card4
-    assert_includes @turn.deck2.cards, @card6
-    assert_includes @turn.deck2.cards, @card7
+    assert_equal 4, @turn.player2.deck.cards.count
+  end
+
+  def test_spoils_of_war_starts_as_empty_array
     assert_equal [], @turn.spoils_of_war
   end
 
@@ -57,8 +54,150 @@ class TurnTest < Minitest::Test
     assert_equal [], @turn.spoils_of_war
     @turn.pile_cards
     assert_equal 2, @turn.spoils_of_war.count
-    assert_includes @turn.deck1.cards, @card1
-    assert_includes @turn.deck2.cards, @card3
+    # assert_includes @turn.player1.deck.cards, @card1
+    # assert_includes @turn.player2.deck.cards, @card3
+  end
+
+  def test_award_spoils_basic
+    @turn.pile_cards
+    assert_equal 2, @turn.spoils_of_war.count
+    @turn.award_spoils(@player1)
+    assert_equal 5, @player1.deck.cards.count#failure saying should be 4 cards
+    assert_equal 3, @player2.deck.cards.count
+    assert_equal 0, @turn.spoils_of_war.count
+  end
+
+  def test_war_turn_type
+    card1 = Card.new(:heart, 'Jack', 11)
+    card2 = Card.new(:heart, '10', 10)
+    card3 = Card.new(:heart, '9', 9)
+    card4 = Card.new(:diamond, 'Jack', 11)
+    card5 = Card.new(:heart, '8', 8)
+    card6 = Card.new(:diamond, 'Queen', 12)
+    card7 = Card.new(:heart, '3', 3)
+    card8 = Card.new(:diamond, '2', 2)
+    deck1 = Deck.new([card1, card2, card5, card8])
+    deck2 = Deck.new([card4, card3, card6, card7])
+    player1 = Player.new("Megan", deck1)
+    player2 = Player.new("Aurora", deck2)
+    turn = Turn.new(player1, player2)
+
+    assert_equal :war, turn.type
+  end
+
+  def test_war_winner
+    card1 = Card.new(:heart, 'Jack', 11)
+    card2 = Card.new(:heart, '10', 10)
+    card3 = Card.new(:heart, '9', 9)
+    card4 = Card.new(:diamond, 'Jack', 11)
+    card5 = Card.new(:heart, '8', 8)
+    card6 = Card.new(:diamond, 'Queen', 12)
+    card7 = Card.new(:heart, '3', 3)
+    card8 = Card.new(:diamond, '2', 2)
+    deck1 = Deck.new([card1, card2, card5, card8])
+    deck2 = Deck.new([card4, card3, card6, card7])
+    player1 = Player.new("Megan", deck1)
+    player2 = Player.new("Aurora", deck2)
+    turn = Turn.new(player1, player2)
+
+    assert_equal player2, turn.winner
+  end
+
+  def test_pile_cards_into_spoils_pile_war
+    card1 = Card.new(:heart, 'Jack', 11)
+    card2 = Card.new(:heart, '10', 10)
+    card3 = Card.new(:heart, '9', 9)
+    card4 = Card.new(:diamond, 'Jack', 11)
+    card5 = Card.new(:heart, '8', 8)
+    card6 = Card.new(:diamond, 'Queen', 12)
+    card7 = Card.new(:heart, '3', 3)
+    card8 = Card.new(:diamond, '2', 2)
+    deck1 = Deck.new([card1, card2, card5, card8])
+    deck2 = Deck.new([card4, card3, card6, card7])
+    player1 = Player.new("Megan", deck1)
+    player2 = Player.new("Aurora", deck2)
+    turn = Turn.new(player1, player2)
+
+    assert_equal [], turn.spoils_of_war
+    turn.pile_cards
+    assert_equal 6, turn.spoils_of_war.count
+  end
+
+  def test_award_spoils_war
+    card1 = Card.new(:heart, 'Jack', 11)
+    card2 = Card.new(:heart, '10', 10)
+    card3 = Card.new(:heart, '9', 9)
+    card4 = Card.new(:diamond, 'Jack', 11)
+    card5 = Card.new(:heart, '8', 8)
+    card6 = Card.new(:diamond, 'Queen', 12)
+    card7 = Card.new(:heart, '3', 3)
+    card8 = Card.new(:diamond, '2', 2)
+    deck1 = Deck.new([card1, card2, card5, card8])
+    deck2 = Deck.new([card4, card3, card6, card7])
+    player1 = Player.new("Megan", deck1)
+    player2 = Player.new("Aurora", deck2)
+    turn = Turn.new(player1, player2)
+
+    turn.pile_cards
+    assert_equal 6, turn.spoils_of_war.count
+    turn.award_spoils(player2)
+    assert_equal 1, player1.deck.cards.count
+    assert_equal 7, player2.deck.cards.count#failure says should be 2 cards
+  end
+
+  def test_mad_turn_type
+    card1 = Card.new(:heart, 'Jack', 11)
+    card2 = Card.new(:heart, '10', 10)
+    card3 = Card.new(:heart, '9', 9)
+    card4 = Card.new(:diamond, 'Jack', 11)
+    card5 = Card.new(:heart, '8', 8)
+    card6 = Card.new(:diamond, '8', 8)
+    card7 = Card.new(:heart, '3', 3)
+    card8 = Card.new(:diamond, '2', 2)
+    deck1 = Deck.new([card1, card2, card5, card8])
+    deck2 = Deck.new([card4, card3, card6, card7])
+    player1 = Player.new("Megan", deck1)
+    player2 = Player.new("Aurora", deck2)
+    turn = Turn.new(player1, player2)
+
+    assert_equal :mad, turn.type
+  end
+
+  def test_mad_winner
+    card1 = Card.new(:heart, 'Jack', 11)
+    card2 = Card.new(:heart, '10', 10)
+    card3 = Card.new(:heart, '9', 9)
+    card4 = Card.new(:diamond, 'Jack', 11)
+    card5 = Card.new(:heart, '8', 8)
+    card6 = Card.new(:diamond, '8', 8)
+    card7 = Card.new(:heart, '3', 3)
+    card8 = Card.new(:diamond, '2', 2)
+    deck1 = Deck.new([card1, card2, card5, card8])
+    deck2 = Deck.new([card4, card3, card6, card7])
+    player1 = Player.new("Megan", deck1)
+    player2 = Player.new("Aurora", deck2)
+    turn = Turn.new(player1, player2)
+
+    assert_equal "No Winner", turn.winner
+  end
+
+  def test_pile_cards_into_spoils_pile_mad
+    card1 = Card.new(:heart, 'Jack', 11)
+    card2 = Card.new(:heart, '10', 10)
+    card3 = Card.new(:heart, '9', 9)
+    card4 = Card.new(:diamond, 'Jack', 11)
+    card5 = Card.new(:heart, '8', 8)
+    card6 = Card.new(:diamond, '8', 8)
+    card7 = Card.new(:heart, '3', 3)
+    card8 = Card.new(:diamond, '2', 2)
+    deck1 = Deck.new([card1, card2, card5, card8])
+    deck2 = Deck.new([card4, card3, card6, card7])
+    player1 = Player.new("Megan", deck1)
+    player2 = Player.new("Aurora", deck2)
+    turn = Turn.new(player1, player2)
+
+    turn.pile_cards
+    assert_equal [], turn.spoils_of_war
   end
 
 end
