88d547388c978dbcf8f51aa398fff3884cd0b706
Dani Coleman <danicoleman00@gmail.com>
2020-06-25 15:18:01 -0600

    Update pile cards method

--- lib/turn.rb
+++ lib/turn.rb
@@ -38,7 +38,7 @@ class Turn
       player1
     elsif player1.deck.rank_of_card_at(2) < player2.deck.rank_of_card_at(2)
       player2
-    else player1.deck.rank_of_card_at(0) == player2.deck.rank_of_card_at(0)
+    elsif player1.deck.rank_of_card_at(0) == player2.deck.rank_of_card_at(0)
       "No Winner"
     end
   end
@@ -48,17 +48,29 @@ class Turn
       @spoils_of_war << player1.deck.remove_card
       @spoils_of_war << player2.deck.remove_card
     elsif type == :war
-      @spoils_of_war.concat player1.deck.cards.shift(3)
-      @spoils_of_war.concat player2.deck.cards.shift(3)
-    else type == :mad
-      player1.deck.cards.shift(3)
-      player2.deck.cards.shift(3)
+      @spoils_of_war << player1.deck.remove_card
+      @spoils_of_war << player1.deck.remove_card
+      @spoils_of_war << player1.deck.remove_card
+      @spoils_of_war << player2.deck.remove_card
+      @spoils_of_war << player2.deck.remove_card
+      @spoils_of_war << player2.deck.remove_card
+    elsif type == :mad
+      player1.deck.remove_card
+      player1.deck.remove_card
+      player1.deck.remove_card
+      player2.deck.remove_card
+      player2.deck.remove_card
+      player2.deck.remove_card
     end
   end
 
   def award_spoils(winner)
-    winner.deck.add_card(spoils_of_war)
-    @spoils_of_war = []
+    if winner == player1 || winner == player2
+      @spoils_of_war << winner.deck.cards
+      winner.deck.cards == winner.deck.cards.flatten
+    else
+      p "No spoils to award"
+    end
   end
 
 end
