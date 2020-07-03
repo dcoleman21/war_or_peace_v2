668ae5909d0f77f8f58d78b3dc398174e7ccff43
Dani Coleman <danicoleman00@gmail.com>
2020-07-02 19:35:31 -0600

    Create deck, players and game instances

--- war_or_peace_runner.rb
+++ war_or_peace_runner.rb
@@ -1,3 +1,34 @@
+require './lib/card'
+require './lib/deck'
+require './lib/player'
 require './lib/war_game'
 
-WarGame.new.start
+suits = [:heart, :spade, :diamond, :club]
+
+standard_deck = []
+
+suits.each do |suit|
+  2.upto(10) do |number|
+    standard_deck << Card.new(:suit, "#{number}", number)
+  end
+  standard_deck << Card.new(:suit, "Jack", 11)
+  standard_deck << Card.new(:suit, "Queen", 12)
+  standard_deck << Card.new(:suit, "King", 13)
+  standard_deck << Card.new(:suit, "Ace", 14)
+end
+
+standard_deck.shuffle!
+
+deck1 = Deck.new([])
+deck2 = Deck.new([])
+
+26.times do
+  deck1.add_card(standard_deck.pop)
+  deck2.add_card(standard_deck.pop)
+end
+
+player1 = Player.new("Megan", deck1)
+player2 = Player.new("Aurora", deck2)
+
+war_game = WarGame.new(player1, player2)
+war_game.start
