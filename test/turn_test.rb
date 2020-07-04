448d2eaab4a767df684d55755337b1b7ac1a4c9c
Dani Coleman <danicoleman00@gmail.com>
2020-07-04 09:31:58 -0600

    Refactor setup for Mad and War

--- test/turn_test.rb
+++ test/turn_test.rb
@@ -73,64 +73,172 @@ class TurnTest < Minitest::Test
   end
 
   def test_war_turn_type_if_top_cards_are_same_rank
-    card1 = Card.new(:heart, 'Jack', 11)
-    card2 = Card.new(:heart, '10', 10)
-    card3 = Card.new(:heart, '9', 9)
-    card4 = Card.new(:diamond, 'Jack', 11)
-    card5 = Card.new(:heart, '8', 8)
-    card6 = Card.new(:diamond, 'Queen', 12)
-    card7 = Card.new(:heart, '3', 3)
-    card8 = Card.new(:diamond, '2', 2)
-    deck1 = Deck.new([card1, card2, card5, card8])
-    deck2 = Deck.new([card4, card3, card6, card7])
-    player1 = Player.new("Megan", deck1)
-    player2 = Player.new("Aurora", deck2)
-    turn = Turn.new(player1, player2)
+    setup_war_turn
+    # card1 = Card.new(:heart, 'Jack', 11)
+    # card2 = Card.new(:heart, '10', 10)
+    # card3 = Card.new(:heart, '9', 9)
+    # card4 = Card.new(:diamond, 'Jack', 11)
+    # card5 = Card.new(:heart, '8', 8)
+    # card6 = Card.new(:diamond, 'Queen', 12)
+    # card7 = Card.new(:heart, '3', 3)
+    # card8 = Card.new(:diamond, '2', 2)
+    # deck1 = Deck.new([card1, card2, card5, card8])
+    # deck2 = Deck.new([card4, card3, card6, card7])
+    # player1 = Player.new("Megan", deck1)
+    # player2 = Player.new("Aurora", deck2)
+    # turn = Turn.new(player1, player2)
 
-    assert_equal :war, turn.type
+    assert_equal :war, @war_turn.type
   end
 
   def test_who_is_the_winner_of_the_war_turn_type
-    card1 = Card.new(:heart, 'Jack', 11)
-    card2 = Card.new(:heart, '10', 10)
-    card3 = Card.new(:heart, '9', 9)
-    card4 = Card.new(:diamond, 'Jack', 11)
-    card5 = Card.new(:heart, '8', 8)
-    card6 = Card.new(:diamond, 'Queen', 12)
-    card7 = Card.new(:heart, '3', 3)
-    card8 = Card.new(:diamond, '2', 2)
-    deck1 = Deck.new([card1, card2, card5, card8])
-    deck2 = Deck.new([card4, card3, card6, card7])
-    player1 = Player.new("Megan", deck1)
-    player2 = Player.new("Aurora", deck2)
-    turn = Turn.new(player1, player2)
+    setup_war_turn
+    # card1 = Card.new(:heart, 'Jack', 11)
+    # card2 = Card.new(:heart, '10', 10)
+    # card3 = Card.new(:heart, '9', 9)
+    # card4 = Card.new(:diamond, 'Jack', 11)
+    # card5 = Card.new(:heart, '8', 8)
+    # card6 = Card.new(:diamond, 'Queen', 12)
+    # card7 = Card.new(:heart, '3', 3)
+    # card8 = Card.new(:diamond, '2', 2)
+    # deck1 = Deck.new([card1, card2, card5, card8])
+    # deck2 = Deck.new([card4, card3, card6, card7])
+    # player1 = Player.new("Megan", deck1)
+    # player2 = Player.new("Aurora", deck2)
+    # turn = Turn.new(player1, player2)
 
-    assert_equal :war, turn.type
-    return deck1.cards[0] && deck2.cards[0]
-    assert_equal player2, turn.winner
+    assert_equal :war, @war_turn.type
+    # return @war_deck1.cards[0] && @war_deck2.cards[0]
+    assert_equal @war_player2, @war_turn.winner
   end
 
   def test_pile_cards_into_spoils_pile_war_turn_type
-    card1 = Card.new(:heart, 'Jack', 11)
-    card2 = Card.new(:heart, '10', 10)
-    card3 = Card.new(:heart, '9', 9)
-    card4 = Card.new(:diamond, 'Jack', 11)
-    card5 = Card.new(:heart, '8', 8)
-    card6 = Card.new(:diamond, 'Queen', 12)
-    card7 = Card.new(:heart, '3', 3)
-    card8 = Card.new(:diamond, '2', 2)
-    deck1 = Deck.new([card1, card2, card5, card8])
-    deck2 = Deck.new([card4, card3, card6, card7])
-    player1 = Player.new("Megan", deck1)
-    player2 = Player.new("Aurora", deck2)
-    turn = Turn.new(player1, player2)
-    turn.pile_cards
-    assert_equal 6, turn.spoils_of_war.count
-    assert_equal 1, turn.player1.deck.cards.count
-    assert_equal 1, turn.player2.deck.cards.count
+    setup_war_turn
+    # card1 = Card.new(:heart, 'Jack', 11)
+    # card2 = Card.new(:heart, '10', 10)
+    # card3 = Card.new(:heart, '9', 9)
+    # card4 = Card.new(:diamond, 'Jack', 11)
+    # card5 = Card.new(:heart, '8', 8)
+    # card6 = Card.new(:diamond, 'Queen', 12)
+    # card7 = Card.new(:heart, '3', 3)
+    # card8 = Card.new(:diamond, '2', 2)
+    # deck1 = Deck.new([card1, card2, card5, card8])
+    # deck2 = Deck.new([card4, card3, card6, card7])
+    # player1 = Player.new("Megan", deck1)
+    # player2 = Player.new("Aurora", deck2)
+    # turn = Turn.new(player1, player2)
+    @war_turn.pile_cards
+    assert_equal 6, @war_turn.spoils_of_war.count
+    assert_equal 1, @war_turn.player1.deck.cards.count
+    assert_equal 1, @war_turn.player2.deck.cards.count
   end
 
   def test_award_spoils_war_turn_type
+    setup_war_turn
+    # card1 = Card.new(:heart, 'Jack', 11)
+    # card2 = Card.new(:heart, '10', 10)
+    # card3 = Card.new(:heart, '9', 9)
+    # card4 = Card.new(:diamond, 'Jack', 11)
+    # card5 = Card.new(:heart, '8', 8)
+    # card6 = Card.new(:diamond, 'Queen', 12)
+    # card7 = Card.new(:heart, '3', 3)
+    # card8 = Card.new(:diamond, '2', 2)
+    # deck1 = Deck.new([card1, card2, card5, card8])
+    # deck2 = Deck.new([card4, card3, card6, card7])
+    # player1 = Player.new("Megan", deck1)
+    # player2 = Player.new("Aurora", deck2)
+    # turn = Turn.new(player1, player2)
+    @war_turn.pile_cards
+    @war_turn.award_spoils(@war_player2)
+    assert_empty @war_turn.spoils_of_war
+    assert_equal 1, @war_turn.player1.deck.cards.count
+    assert_equal 7, @war_turn.player2.deck.cards.count
+  end
+
+  def test_mad_turn_type_if_top_and_third_card_have_same_rank
+    setup_mad_turn
+    # card1 = Card.new(:heart, 'Jack', 11)
+    # card2 = Card.new(:heart, '10', 10)
+    # card3 = Card.new(:heart, '9', 9)
+    # card4 = Card.new(:diamond, 'Jack', 11)
+    # card5 = Card.new(:heart, '8', 8)
+    # card6 = Card.new(:diamond, '8', 8)
+    # card7 = Card.new(:heart, '3', 3)
+    # card8 = Card.new(:diamond, '2', 2)
+    # deck1 = Deck.new([card1, card2, card5, card8])
+    # deck2 = Deck.new([card4, card3, card6, card7])
+    # player1 = Player.new("Megan", deck1)
+    # player2 = Player.new("Aurora", deck2)
+    # turn = Turn.new(player1, player2)
+
+    assert_equal :mad, @mad_turn.type
+  end
+
+  def test_who_is_the_winner_of_the_mad_turn_type
+    setup_mad_turn
+    # card1 = Card.new(:heart, 'Jack', 11)
+    # card2 = Card.new(:heart, '10', 10)
+    # card3 = Card.new(:heart, '9', 9)
+    # card4 = Card.new(:diamond, 'Jack', 11)
+    # card5 = Card.new(:heart, '8', 8)
+    # card6 = Card.new(:diamond, '8', 8)
+    # card7 = Card.new(:heart, '3', 3)
+    # card8 = Card.new(:diamond, '2', 2)
+    # deck1 = Deck.new([card1, card2, card5, card8])
+    # deck2 = Deck.new([card4, card3, card6, card7])
+    # player1 = Player.new("Megan", deck1)
+    # player2 = Player.new("Aurora", deck2)
+    # turn = Turn.new(player1, player2)
+
+    assert_equal :mad, @mad_turn.type
+    # return @mad_deck1.cards[0..2] && @mad_deck2.cards[0..2]
+    assert_equal "No Winner", @mad_turn.winner
+  end
+
+  def test_pile_cards_into_spoils_pile_mad
+    setup_mad_turn
+    # card1 = Card.new(:heart, 'Jack', 11)
+    # card2 = Card.new(:heart, '10', 10)
+    # card3 = Card.new(:heart, '9', 9)
+    # card4 = Card.new(:diamond, 'Jack', 11)
+    # card5 = Card.new(:heart, '8', 8)
+    # card6 = Card.new(:diamond, '8', 8)
+    # card7 = Card.new(:heart, '3', 3)
+    # card8 = Card.new(:diamond, '2', 2)
+    # deck1 = Deck.new([card1, card2, card5, card8])
+    # deck2 = Deck.new([card4, card3, card6, card7])
+    # player1 = Player.new("Megan", deck1)
+    # player2 = Player.new("Aurora", deck2)
+    # turn = Turn.new(player1, player2)
+    @mad_turn.pile_cards
+    assert_equal 0, @mad_turn.spoils_of_war.count
+    assert_equal 1, @mad_turn.player1.deck.cards.count
+    assert_equal 1, @mad_turn.player2.deck.cards.count
+  end
+
+  def test_award_spoils_returns_nothing_if_mad_turn_type
+    setup_mad_turn
+    # card1 = Card.new(:heart, 'Jack', 11)
+    # card2 = Card.new(:heart, '10', 10)
+    # card3 = Card.new(:heart, '9', 9)
+    # card4 = Card.new(:diamond, 'Jack', 11)
+    # card5 = Card.new(:heart, '8', 8)
+    # card6 = Card.new(:diamond, '8', 8)
+    # card7 = Card.new(:heart, '3', 3)
+    # card8 = Card.new(:diamond, '2', 2)
+    # deck1 = Deck.new([card1, card2, card5, card8])
+    # deck2 = Deck.new([card4, card3, card6, card7])
+    # player1 = Player.new("Megan", deck1)
+    # player2 = Player.new("Aurora", deck2)
+    # turn = Turn.new(player1, player2)
+    @mad_turn.pile_cards
+    @mad_turn.award_spoils("No Winner")
+    assert_empty @mad_turn.spoils_of_war
+    assert_equal 1, @mad_turn.player1.deck.cards.count
+    assert_equal 1, @mad_turn.player2.deck.cards.count
+  end
+
+  def setup_war_turn
     card1 = Card.new(:heart, 'Jack', 11)
     card2 = Card.new(:heart, '10', 10)
     card3 = Card.new(:heart, '9', 9)
@@ -141,17 +249,12 @@ class TurnTest < Minitest::Test
     card8 = Card.new(:diamond, '2', 2)
     deck1 = Deck.new([card1, card2, card5, card8])
     deck2 = Deck.new([card4, card3, card6, card7])
-    player1 = Player.new("Megan", deck1)
-    player2 = Player.new("Aurora", deck2)
-    turn = Turn.new(player1, player2)
-    turn.pile_cards
-    turn.award_spoils(player2)
-    assert_empty turn.spoils_of_war
-    assert_equal 1, turn.player1.deck.cards.count
-    assert_equal 7, turn.player2.deck.cards.count
+    @war_player1 = Player.new("Megan", deck1)
+    @war_player2 = Player.new("Aurora", deck2)
+    @war_turn = Turn.new(@war_player1, @war_player2)
   end
 
-  def test_mad_turn_type_if_top_and_third_card_have_same_rank
+  def setup_mad_turn
     card1 = Card.new(:heart, 'Jack', 11)
     card2 = Card.new(:heart, '10', 10)
     card3 = Card.new(:heart, '9', 9)
@@ -162,72 +265,9 @@ class TurnTest < Minitest::Test
     card8 = Card.new(:diamond, '2', 2)
     deck1 = Deck.new([card1, card2, card5, card8])
     deck2 = Deck.new([card4, card3, card6, card7])
-    player1 = Player.new("Megan", deck1)
-    player2 = Player.new("Aurora", deck2)
-    turn = Turn.new(player1, player2)
-
-    assert_equal :mad, turn.type
-  end
-
-  def test_who_is_the_winner_of_the_mad_turn_type
-    card1 = Card.new(:heart, 'Jack', 11)
-    card2 = Card.new(:heart, '10', 10)
-    card3 = Card.new(:heart, '9', 9)
-    card4 = Card.new(:diamond, 'Jack', 11)
-    card5 = Card.new(:heart, '8', 8)
-    card6 = Card.new(:diamond, '8', 8)
-    card7 = Card.new(:heart, '3', 3)
-    card8 = Card.new(:diamond, '2', 2)
-    deck1 = Deck.new([card1, card2, card5, card8])
-    deck2 = Deck.new([card4, card3, card6, card7])
-    player1 = Player.new("Megan", deck1)
-    player2 = Player.new("Aurora", deck2)
-    turn = Turn.new(player1, player2)
-
-    assert_equal :mad, turn.type
-    return deck1.cards[0..2] && deck2.cards[0..2]
-    assert_equal "No Winner", turn.winner
-  end
-
-  def test_pile_cards_into_spoils_pile_mad
-    card1 = Card.new(:heart, 'Jack', 11)
-    card2 = Card.new(:heart, '10', 10)
-    card3 = Card.new(:heart, '9', 9)
-    card4 = Card.new(:diamond, 'Jack', 11)
-    card5 = Card.new(:heart, '8', 8)
-    card6 = Card.new(:diamond, '8', 8)
-    card7 = Card.new(:heart, '3', 3)
-    card8 = Card.new(:diamond, '2', 2)
-    deck1 = Deck.new([card1, card2, card5, card8])
-    deck2 = Deck.new([card4, card3, card6, card7])
-    player1 = Player.new("Megan", deck1)
-    player2 = Player.new("Aurora", deck2)
-    turn = Turn.new(player1, player2)
-    turn.pile_cards
-    assert_equal 0, turn.spoils_of_war.count
-    assert_equal 1, turn.player1.deck.cards.count
-    assert_equal 1, turn.player2.deck.cards.count
-  end
-
-  def test_award_spoils_returns_nothing_if_mad_turn_type
-    card1 = Card.new(:heart, 'Jack', 11)
-    card2 = Card.new(:heart, '10', 10)
-    card3 = Card.new(:heart, '9', 9)
-    card4 = Card.new(:diamond, 'Jack', 11)
-    card5 = Card.new(:heart, '8', 8)
-    card6 = Card.new(:diamond, '8', 8)
-    card7 = Card.new(:heart, '3', 3)
-    card8 = Card.new(:diamond, '2', 2)
-    deck1 = Deck.new([card1, card2, card5, card8])
-    deck2 = Deck.new([card4, card3, card6, card7])
-    player1 = Player.new("Megan", deck1)
-    player2 = Player.new("Aurora", deck2)
-    turn = Turn.new(player1, player2)
-    turn.pile_cards
-    turn.award_spoils("No Winner")
-    assert_empty turn.spoils_of_war
-    assert_equal 1, turn.player1.deck.cards.count
-    assert_equal 1, turn.player2.deck.cards.count
+    @mad_player1 = Player.new("Megan", deck1)
+    @mad_player2 = Player.new("Aurora", deck2)
+    @mad_turn = Turn.new(@mad_player1, @mad_player2)
   end
 
 end
